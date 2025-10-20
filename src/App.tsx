a9633aa9fcc9750b38639d8f8e3bb37d8688cd07
b9348 <1119348319@qq.com>
2025-10-21 03:16:06 +0800

    移除未使用的 FloatingNotification 组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { FloatingNotification } from "@/components/common/FloatingNotification";
 import Index from "./pages/home";
 import Assessment from "./pages/assessment";
 import Results from "./pages/results";
@@ -19,7 +18,6 @@ const App = () => (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster richColors />
-        <FloatingNotification />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
